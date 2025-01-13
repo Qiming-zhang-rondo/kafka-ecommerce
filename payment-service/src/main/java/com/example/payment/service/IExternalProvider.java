4155e138a9c721b625354af52fbb73da2539c35b
Qiming-zhang-rondo <83908874+Qiming-zhang-rondo@users.noreply.github.com>
2025-01-13 20:45:28 +0800

    Delete payment-service directory

--- payment-service/src/main/java/com/example/payment/service/IExternalProvider.java
+++
@@ -1,8 +0,0 @@
-package com.example.payment.service;
-
-import com.example.common.integration.PaymentIntent;
-import com.example.common.integration.PaymentIntentCreateOptions;
-
-public interface IExternalProvider {
-    PaymentIntent create(PaymentIntentCreateOptions options);
-}
