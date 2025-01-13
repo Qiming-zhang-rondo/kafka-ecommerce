f5c552a01663d83a0679e331d79cf97110041765
Qiming-zhang-rondo <83908874+Qiming-zhang-rondo@users.noreply.github.com>
2025-01-13 20:45:35 +0800

    Delete paymentProvider-service directory

--- paymentProvider-service/src/main/java/com/example/paymentProvider/service/IPaymentProvider.java
+++
@@ -1,8 +0,0 @@
-package com.example.paymentProvider.service;
-
-import com.example.common.integration.PaymentIntent;
-import com.example.common.integration.PaymentIntentCreateOptions;
-
-public interface IPaymentProvider {
-    PaymentIntent processPaymentIntent(PaymentIntentCreateOptions options);
-}
